2ee564dbd2eb1b4df05666465f0bfe28806bff21
Lucas Bender <lucas@powervending.com.br>
2024-08-07 21:56:48 -0300

    More Continue suggestions

--- lib/open_project/mock_auth/engine.rb
+++ lib/open_project/mock_auth/engine.rb
@@ -1,23 +1,30 @@
 # Prevent load-order problems in case openproject-plugins is listed after a plugin in the Gemfile
 # or not at all
+# Prevent load-order problems in case openproject-plugins is listed after a plugin in the Gemfile or not at all
 require 'open_project/plugins'
 require 'omniauth/strategies/mock_auth'
 
-module OpenProject::MockAuth
-  class Engine < ::Rails::Engine
-    engine_name :openproject_mock_auth
+module OpenProjectMockAuthEngine
+  def self.configure
+    OmniAuth.config.add_strategy :mock_auth, 'omniauth/strategies/mock_auth'
+  end
 
-    include OpenProject::Plugins::ActsAsOpEngine
-    extend OpenProject::Plugins::AuthPlugin
+  configure
+end
 
-    register 'openproject-mock_auth',
-             :author_url => 'http://finn.de',
-             :requires_openproject => '>= 3.1.0pre1'
+class MockAuth::Engine < ::Rails::Engine
+  engine_name :openproject_mock_auth
 
-    register_auth_providers do
-      strategy :mock_auth do
-        [{name: 'mock_auth', display_name: 'Test'}]
-      end
+  include OpenProject::Plugins::ActsAsOpEngine
+  extend OpenProject::Plugins::AuthPlugin
+
+  register 'openproject-mock_auth',
+           :author_url => 'http://finn.de',
+           :requires_openproject => '>= 3.1.0pre1'
+
+  register_auth_providers do
+    strategy :mock_auth do
+      [{name: 'mock_auth', display_name: 'Test'}]
     end
   end
 end
